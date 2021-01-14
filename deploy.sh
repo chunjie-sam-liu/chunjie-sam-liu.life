42f3dcc5abf06663f5574749b5390c8628ef53b4
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2021-01-14 15:19:47 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: 2021-01-14 15:17:20
+# @DESCRIPTION:
+
+# build blogdown in shell.
+R -e 'blogdown::build_site()'
